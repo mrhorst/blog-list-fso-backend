c5c11781ca39a8e7d405b9dfc7063979e30cb19e
Matheus Horst <mhorst31@gmail.com>
2022-12-31 17:16:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
